c1495c3d822733a24164e5c42296332f77bfc87a
Eiji Hachiya <hachiyae@gmail.com>
2017-10-29 16:25:14 +0900

    nginxにまかせる
    毎回DBを参照していると遅い

--- ruby/app.rb
+++ ruby/app.rb
@@ -316,9 +316,10 @@ class App < Sinatra::Base
     end
 
     if !avatar_name.nil? && !avatar_data.nil?
-      statement = db.prepare('INSERT INTO image (name, data) VALUES (?, ?)')
-      statement.execute(avatar_name, avatar_data)
-      statement.close
+      path = File.expand_path('../../public', __FILE__) + "/icons"
+      File.open(avatar_name, 'w') do |f|
+        f.print avatar_data
+      end
       statement = db.prepare('UPDATE user SET avatar_icon = ? WHERE id = ?')
       statement.execute(avatar_name, user['id'])
       statement.close
@@ -331,21 +332,6 @@ class App < Sinatra::Base
     end
 
     redirect '/', 303
-  end
-
-  get '/icons/:file_name' do
-    file_name = params[:file_name]
-    # FIXME: 画像はすべて静的ファイルにする
-    statement = db.prepare('SELECT * FROM image WHERE name = ?')
-    row = statement.execute(file_name).first
-    statement.close
-    ext = file_name.include?('.') ? File.extname(file_name) : ''
-    mime = ext2mime(ext)
-    if !row.nil? && !mime.empty?
-      content_type mime
-      return row['data']
-    end
-    404
   end
 
   get '/debug/write' do
