89788c3194423500dff7aa126abf7ffa2e8d61fa
Eiji Hachiya <hachiyae@gmail.com>
2017-10-29 17:01:35 +0000

    [Ruby] get db_client via Thread
    score 142894

--- ruby/app.rb
+++ ruby/app.rb
@@ -352,15 +352,19 @@ class App < Sinatra::Base
 
   def db
     return @db_client if defined?(@db_client)
+    @db_client = Thread.current[:db]
+    if @db_client.nil?
+      @db_client = Mysql2::Client.new(
+        host: ENV.fetch('ISUBATA_DB_HOST') { 'localhost' },
+        port: ENV.fetch('ISUBATA_DB_PORT') { '3306' },
+        username: ENV.fetch('ISUBATA_DB_USER') { 'root' },
+        password: ENV.fetch('ISUBATA_DB_PASSWORD') { '' },
+        database: 'isubata',
+        encoding: 'utf8mb4'
+      )
+      Thread.current[:db] = @db_client
+    end
 
-    @db_client = Mysql2::Client.new(
-      host: ENV.fetch('ISUBATA_DB_HOST') { 'localhost' },
-      port: ENV.fetch('ISUBATA_DB_PORT') { '3306' },
-      username: ENV.fetch('ISUBATA_DB_USER') { 'root' },
-      password: ENV.fetch('ISUBATA_DB_PASSWORD') { '' },
-      database: 'isubata',
-      encoding: 'utf8mb4'
-    )
     @db_client.query('SET SESSION sql_mode=\'TRADITIONAL,NO_AUTO_VALUE_ON_ZERO,ONLY_FULL_GROUP_BY\'')
     @db_client
   end
